199c77dbe01f89f30627e7aeb1ecb00a46501a16
Gleb Kalinin <glebstuck@yandex.ru>
2021-12-19 18:47:11 +0500

    added additional files

---
+++ src/EventSubscriber/TwigEventSubscriber.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Repository\CommentsRepository;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Twig\Environment;
+
+class TwigEventSubscriber implements EventSubscriberInterface
+{
+    private Environment $twig;
+    private CommentsRepository $commentsRepository;
+
+    /**
+     * @param Environment $twig
+     * @param CommentsRepository $commentsRepository
+     */
+    public function __construct(Environment $twig, CommentsRepository $commentsRepository)
+    {
+        $this->twig = $twig;
+        $this->commentsRepository = $commentsRepository;
+    }
+
+
+    public function onKernelController(ControllerEvent $event)
+    {
+        $this->twig->addGlobal('header', [
+            [
+                'name' => 'Об авторе',
+                'href' => 'author'
+            ],
+            [
+                'name' => 'Админ панель',
+                'href' => 'admin'
+            ],
+            [
+                'name' => 'Новости',
+                'href' => 'news'
+            ],
+            [
+                'name' => 'Статьи',
+                'href' => 'index'
+            ],
+        ]);
+    }
+
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'kernel.controller' => 'onKernelController',
+        ];
+    }
+}
